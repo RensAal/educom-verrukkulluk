68cba1155f647f9f73e44a0b6b6d71b3bdb5f242
RensAal <rens_aal@msn.com>
2022-02-22 15:29:36 +0100

    artikel (#5)

--- lib/artikel.php
+++ lib/artikel.php
@@ -3,17 +3,19 @@
 class artikel {
 
     private $connection;
+    private $table = "artikelen";
 
     public function __construct($connection) {
         $this->connection = $connection;
     }
   
-    public function selecteerArtikel($artikel_id) {
+    public function selecteerArtikel($artikel_ID) {
 
-        $sql = "select * from artikel where id = $artikel_id";
+        $sql = "select * from $this->table where ID = $artikel_ID";
+        echo $sql;
         
         $result = mysqli_query($this->connection, $sql);
-        $artikel = mysql_fetch_array($result, MYSQLI_ASSOC);
+        $artikel = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
         return($artikel);
 
