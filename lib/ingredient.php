9c67807d8f5ffdb23e5b31670d97d0f06a387864
RensAal <rens_aal@msn.com>
2022-02-23 12:03:33 +0100

    ingredient (#9)

--- lib/ingredient.php
+++ lib/ingredient.php
@@ -15,50 +15,20 @@ class ingredient {
         
         $result = mysqli_query($this->connection, $sql);
 
-        $i = 0;
         while($ingredient = mysqli_fetch_array($result, MYSQLI_ASSOC)){
             
             $artikel_ID = $ingredient['artikel_ID'];
-            $art = new artikel($this->connection);
-            $artikel = $art->selecteerArtikel($artikel_ID);
+            $artikel = new artikel($this->connection);
+            $artikel_array = $artikel->selecteerArtikel($artikel_ID);
             
-            $ingredienten[$i] = $ingredient;
-            $ingredienten[$i] = $this->berekenPrijs($ingredienten[$i], $artikel);
-            $ingredienten[$i] = $this->berekenCalorien($ingredienten[$i], $artikel);
-            
-            $i++;
+            $ingredienten[] = $ingredient + ['artikel' => $artikel_array];
+
         }
 
         return($ingredienten);
     }
 
-    private function berekenPrijs($ingredient, $artikel) {
-        $artikel_prijs = $artikel['prijs'];
-        $standaard_hoeveelheid = $artikel['standaard_hoeveelheid'];
 
-        $hoeveelheid = $ingredient['hoeveelheid'];
-
-        $prijs = ceil($hoeveelheid / $standaard_hoeveelheid) * $artikel_prijs;
-
-        $ingredient = $ingredient + ['prijs'=> $prijs];
-            
-        return($ingredient);
-    }
-
-
-    private function berekenCalorien($ingredient, $artikel) {
-        $artikel_calorien = $artikel['caloriën'];
-        $standaard_hoeveelheid = $artikel['standaard_hoeveelheid'];
-
-        $hoeveelheid = $ingredient['hoeveelheid'];
-
-        $calorien = ($hoeveelheid / $standaard_hoeveelheid) * $artikel_calorien;
-
-        $ingredient = $ingredient + ['calorien'=> $calorien];
-            
-        return($ingredient);
-
-    }
 
 
 }
